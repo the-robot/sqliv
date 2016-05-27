26cf24a07fb3ffa412207aee63017e6954b85112
the-robot <the-robot@users.noreply.github.com>
2016-05-28 01:00:59 +0800

    Update sqlivul.py

--- sqlivul.py
+++ sqlivul.py
@@ -156,7 +156,7 @@ class sqlscan():
             error_msg = {
                         "mysql_error_1":"You have an error in your SQL syntax",
                         "mysql_error_2":"supplied argument is not a valid MySQL result resource",
-                        "mysql_error_3":"check the manual that corresponds to your MySQL"
+                        "mysql_error_3":"check the manual that corresponds to your MySQL",
                         "mysql_error_4":"mysql_fetch_array(): supplied argument is not a valid MySQL",
                         "mssql_error_1":"Microsoft OLE DB Provider for ODBC Drivers error"
                         }
