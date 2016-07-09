d8de22b0924ef7fe2676a8ca39176c9d8d67b73c
the-robot <the-robot@users.noreply.github.com>
2016-07-09 22:35:36 +0800

    Hades.y2k

---
+++ ext/pysearch.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# 09/07/2016
+# Hades.y2k (github.com/Hadesy2k)
+# official.hadesy2k@protonmail.com
+# GPL <3.0>
+
+import sys
+from google import search
+
+
+class main:
+    def __init__(self):
+        print "\nEnter SQLi Dork without 'inurl:'"
+        query = raw_input("Dork: ")
+        query = "inurl:" + query
+        pages = input("Enter number of pages: ")
+        print  # Printing empty new line
+        filename = "sites.txt"  # File will save as 'sites.txt'
+
+        if query != '' and pages != '':
+            self.dork(query, pages, filename)
+
+    def dork(self, query, pages, filename):
+        print "[+] Googling for %s " % query
+        urlList = []
+
+        for url in search(query, stop=pages):
+            urlList.append(url)
+
+        if len(urlList) != 0:
+            print "Result: %i" % len(urlList)
+            output = file(filename, "w")
+            for url in urlList:
+                output.write(url + "\n")
+            output.close()
+        else:
+            print "No result found"
+            sys.exit()
+
+if __name__ == "__main__":
+    main()
