7c8c8db44a42a7b9c8fc18d3b2cab3cb3deab68b
the-robot <the-robot@users.noreply.github.com>
2016-07-09 18:18:33 +0800

    Update pydorker.py

--- pydorker.py
+++ pydorker.py
@@ -3,30 +3,18 @@
 # Just input the dork and number of pages,
 # the script will take care of the rest.
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Name : Google Dorker.py
 # Version : beta
 # Author : Anubis
 # Contact : z3r0.mhu@gmail.com
 # Facebook : www.facebook.com/zerouplink
-#-----------------------------------------------------
+# -----------------------------------------------------
 
 ###########################################################
 
-import time,sys
-
-class bcolors:
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    OKGREEN = '\033[92m'
-    ENDC = '\033[0m'
-    WARNING = '\033[93m'
-
-RED = bcolors.RED
-WHITE = bcolors.BOLD
-GREEN = bcolors.OKGREEN
-YELLOW = bcolors.WARNING
-END = bcolors.ENDC
+import time
+import sys
 
 try:
     from pygoogle import pygoogle
@@ -34,49 +22,53 @@ except ImportError:
     print "[!] Pygoogle file not found!"
     print "[!] Program can't continue"
     print "[!] Get pygoogle file from this link"
-    print "\n\t\t[ http://pastebin.com/Q0xVF2nV ]"
+    print "[ http://pastebin.com/Q0xVF2nV ]"
+
 
 class dorker():
-
     def __init__(self):
 
-        search_dork = raw_input(WHITE + "\nEnter Dork : " + END)
-        pages = input(WHITE + "Enter number of pages : " + END)
+        search_dork = raw_input("\nEnter Dork : ")
+        pages = input("Enter number of pages : ")
         print
-        f_output = "googled.txt"
+        f_output = "sites.txt"
 
         if search_dork != '' and pages != '':
-            self.dork(search_dork,pages,f_output)
+            self.dork(search_dork, pages, f_output)
 
-    def dork(self,search_term,p,output):
-        print YELLOW + "[+] " + END + WHITE + "Searching for " + END + "%s " % search_term
+    def dork(self, search_term, p, output):
+        print "[+] Searching for %s " % search_term
         gs = pygoogle(search_term)
         gs.pages = p
-        print YELLOW + "[+] " + END + WHITE + "Results Found : " + END + "%s " % (gs.get_result_count())
-        if gs.get_result_count() == 0: print RED + "[-] " + END + WHITE + "No Results Found" + END; time.sleep(1); sys.exit()
+        print "[+] Results Found : %s " % (gs.get_result_count())
+        if gs.get_result_count() == 0:
+            print "[-] No Results Found"
+            time.sleep(1)
+            sys.exit()
 
-        print YELLOW + "[+] " + END + WHITE + "Fetching " + END + "[%s] Results " % (gs.get_result_count())
+        print "[+] Fetching [%s] Results" % (gs.get_result_count())
         url_list = gs.get_urls()
 
         if len(url_list) == 0:
-            print YELLOW + "[!] " + END + WHITE + "Got 0 URLs" + END
-            print RED + "[!] " + END + WHITE + "Nothing to save" + END
+            print "[!] Got 0 URLs"
+            print "[!] Nothing to save"
             time.sleep(1)
             sys.exit()
-            
-        elif len(url_list) > 1:
-            print YELLOW + "[+] " + END + WHITE + "Got " + END + "[%s] URLs" % (len(url_list))
-            print YELLOW + "[+] " + END + WHITE + "Writing URLs to " + END + "[%s] " % (output)
 
-            with open(output,'w') as w_file:
-                for i in url_list: w_file.write(i+'\n')
-            print YELLOW + "[+] " + END + WHITE + "URLs saved to " + END + "[%s] " % (output)
+        elif len(url_list) > 1:
+            print "[+] Got [%s] URLs" % (len(url_list))
+            print "[+] Writing URLs to [%s] " % (output)
+
+            with open(output, 'w') as w_file:
+                for i in url_list:
+                    w_file.write(i + '\n')
+            print "[+] URLs saved to [%s] " % (output)
 
             time.sleep(2)
 
 
 def main():
-    google = dorker()
+    dorker()
 
 
 if __name__ == '__main__':
