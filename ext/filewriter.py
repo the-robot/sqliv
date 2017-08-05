1f1b925b7bd06cb8600858cd549f3492b2aa8e12
the-robot <the-robot@users.noreply.github.com>
2017-08-05 21:04:14 +0800

    Delete filewriter.py

--- ext/filewriter.py
+++
@@ -1,8 +0,0 @@
-def writeAsText(fileName, array):
-    """ write the array into text file """
-    ioWriter = open(fileName, 'w')
-    for item in array:
-        print item
-        ioWriter.write(item + "\n")
-    ioWriter.close()
-    print "Saved into " + fileName
