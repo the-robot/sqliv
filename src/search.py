e804ab7997f20f4646f6ce29397781b9c55051df
blackvkng <blackvkng@yandex.com>
2017-08-29 03:38:19 +0300

    Bing search engine added to options

--- src/search.py
+++ src/search.py
@@ -1,9 +1,12 @@
 # search vulnerabilities by dock
 
 import sys
+
+from libs import bing
 from libs import google
 from urllib2 import HTTPError, URLError
 
+bingsearch = bing.Bing()
 
 class Search:
     """basic search class that can be inherited by other search agents like Google, Yandex"""
@@ -11,9 +14,9 @@ class Search:
 
 class Google(Search):
     """search vulnerabilities by Google's dock"""
-
     def search(self, query, pages=10):
         """search and return an array of urls"""
+
         urls = []
 
         try:
@@ -27,3 +30,14 @@ class Google(Search):
             exit("Unknown error occurred")
 
         return urls
+
+class Bing(Search):
+    def search(self, query, pages=10):
+        try:
+            return bingsearch.search(query, stop=pages)
+        except HTTPError:
+            exit("[503] Service Unreachable")
+        except URLError:
+            exit("[504] Gateway Timeout")
+        except:
+            exit("Unknown error occurred")
