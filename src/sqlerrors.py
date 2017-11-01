05a32aa2c4f0768d536a0cb5b2d93d9580b0469b
the-robot <the-robot@users.noreply.github.com>
2017-11-02 01:50:19 +0800

    more db detection

--- src/sqlerrors.py
+++ src/sqlerrors.py
@@ -1,18 +1,18 @@
 import re
 
-# credit (https://github.com/Ekultek)
-sql_errors = {
-    "MySQL": (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r".*MySQL Query fail.*", r"Fatal error.*:.*"),
-    "PostgreSQL": (r"PostgreSQL.*ERROR", r"Warning.*\Wpg_.*"),
-    "Microsoft SQL Server": (r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Msg \d+, Level \d+, State \d+", r"Unclosed quotation mark after the character string"),
-    "Microsoft Access": (r"Microsoft Access Driver", r"Access Database Engine", r"Microsoft JET Database Engine", r"Fatal error.*Uncaught exception.*"),
-    "Oracle": (r"\bORA-[0-9][0-9][0-9][0-9]", r"Oracle error"),
-    "IBM DB2": (r"CLI Driver.*DB2", r"DB2 SQL error"),
-    "SQLite": (r"SQLite/JDBCDriver",r"System.Data.SQLite.SQLiteException"),
-    "MariaDB": (r".*SQL syntax;.*MariaDB.*"),
-    #"Sybase": (r"(?\i)Warning.*sybase.*")
-}
 
+sql_errors = {
+    "MySQL": (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"MySQL Query fail.*"),
+    "PostgreSQL": (r"PostgreSQL.*ERROR", r"Warning.*\Wpg_.*", r"Warning.*PostgreSQL"),
+    "Microsoft SQL Server": (r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Warning.*odbc_.*", r"Warning.*mssql_", r"Msg \d+, Level \d+, State \d+", r"Unclosed quotation mark after the character string", r"Microsoft OLE DB Provider for ODBC Drivers"),
+    "Microsoft Access": (r"Microsoft Access Driver", r"Access Database Engine", r"Microsoft JET Database Engine", r".*Syntax error.*query expression"),
+    "Oracle": (r"\bORA-[0-9][0-9][0-9][0-9]", r"Oracle error", r"Warning.*oci_.*", "Microsoft OLE DB Provider for Oracle"),
+    "IBM DB2": (r"CLI Driver.*DB2", r"DB2 SQL error"),
+    "SQLite": (r"SQLite/JDBCDriver", r"System.Data.SQLite.SQLiteException"),
+    "MariaDB": (r"SQL syntax.*MariaDB server",),
+    "Informix": (r"Warning.*ibase_.*", r"com.informix.jdbc"),
+    "Sybase": (r"Warning.*sybase.*", r"Sybase message")
+}
 
 def check(html):
     """check SQL error is in HTML or not"""
