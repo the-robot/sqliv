5d07c41ba8f10cb7aca94d7aa43796eccdaf06a9
the-robot <the-robot@users.noreply.github.com>
2017-11-15 09:26:59 +0800

    update

--- src/sqlerrors.py
+++ src/sqlerrors.py
@@ -2,14 +2,13 @@ import re
 
 # thanks Ekultek (https://github.com/Ekultek) for giving better idea of detection
 sql_errors = {
-    "MySQL": (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"MySQL Query fail.*"),
+    "MySQL": (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"MySQL Query fail.*", r"SQL syntax.*MariaDB server"),
     "PostgreSQL": (r"PostgreSQL.*ERROR", r"Warning.*\Wpg_.*", r"Warning.*PostgreSQL"),
     "Microsoft SQL Server": (r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Warning.*odbc_.*", r"Warning.*mssql_", r"Msg \d+, Level \d+, State \d+", r"Unclosed quotation mark after the character string", r"Microsoft OLE DB Provider for ODBC Drivers"),
     "Microsoft Access": (r"Microsoft Access Driver", r"Access Database Engine", r"Microsoft JET Database Engine", r".*Syntax error.*query expression"),
     "Oracle": (r"\bORA-[0-9][0-9][0-9][0-9]", r"Oracle error", r"Warning.*oci_.*", "Microsoft OLE DB Provider for Oracle"),
     "IBM DB2": (r"CLI Driver.*DB2", r"DB2 SQL error"),
     "SQLite": (r"SQLite/JDBCDriver", r"System.Data.SQLite.SQLiteException"),
-    "MariaDB": (r"SQL syntax.*MariaDB server",),
     "Informix": (r"Warning.*ibase_.*", r"com.informix.jdbc"),
     "Sybase": (r"Warning.*sybase.*", r"Sybase message")
 }
